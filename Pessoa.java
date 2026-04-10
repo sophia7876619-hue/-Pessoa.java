9f015ad1a8426fe73afe5e7c8fde140865c40118
sophia7876619-hue <sophia.7876619@aluno.mg.gov.br>
2026-04-10 12:57:03 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,11 @@
+________________________________________________________package Testes;
+public class Pessoa {
+String nome;
+int idade;
+        public void caminhar() {
+        System.out.println("caminhando...");
+        }
+        public void dormir() {
+        System.out.println("dormir...");
+        }
+}
